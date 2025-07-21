83f6b210ef061aedab8f0976b0b4562c43a79c58
Veronika Golovkova <veronika87979@gmail.com>
2025-07-21 11:45:29 +0300

    feat: implement reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const numStr = String(Math.abs(n));
+
+  const reverseNumArr = numStr.split('').reverse();
+
+  const reverseNum = Number(reverseNumArr.join(''));
+
+  return reverseNum;
 };
